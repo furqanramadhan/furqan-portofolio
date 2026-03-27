a0e223f8c8066b44f7e74fe7da9929ce6d256130
kelfinofarelino <farelinokelvin@gmail.com>
2026-03-28 02:15:11 +0700

    add weather slug

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -2,7 +2,7 @@ export default function WeatherPage() {
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', backgroundColor: '#0A0C10' }}>
       <iframe
-        src="https://kenar-sky-guardian.streamlit.app/?embed=true"
+        src="https://diy-weather-pipeline-slyoqb5t8jbhsr2pl8nwn5.streamlit.app/"
         style={{
           width: '100%',
           height: '100%',
